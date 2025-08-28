637535510ad61a3e97fcb9d08069b87b111cc86d
Merlin Valdez <163349473+merlinvaldez@users.noreply.github.com>
2025-08-27 22:08:31 -0400

    feat: Add initial test file for pre-commit hook

---
+++ test-precommit.ts
@@ -0,0 +1,8 @@
+// Test file for pre-commit hook
+const badlyFormatted = 'hello world';
+const unused_variable = 42;
+function testFunction() {
+  console.log(badlyFormatted);
+}
+
+export default testFunction;
